3f704ad146435896f3fb8ee73cefdb52d08743fc
rezazr22 <reza@mahapatih.id>
2020-12-10 19:00:46 +0700

    add owned pokemon

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,23 @@
+import React from "react";
+
 import { uppercaseFirstLetter } from "../helpers";
 
 function List({ pokemon, handleClickPokemon }) {
+  const [ownedPokemons, setOwnedPokemons] = React.useState(() =>
+    JSON.parse(localStorage.getItem("ownedPokemon"))
+  );
+
   return (
     <li onClick={(e) => handleClickPokemon(e, pokemon)}>
       <div className="list-name">{uppercaseFirstLetter(pokemon.name)}</div>
-      <div className="list-details">Owned: 0</div>
+      <div className="list-details">
+        Owned:{" "}
+        {ownedPokemons
+          ? ownedPokemons.filter(
+              (ownedPokemon) => ownedPokemon.name === pokemon.name
+            ).length
+          : 0}
+      </div>
     </li>
   );
 }

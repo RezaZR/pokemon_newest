4d2fd720d85c41dd42cffecd48489ce32b6376a2
Reza Z. Ramadan <rezazramadhan@gmail.com>
2020-12-12 16:21:30 +0700

    add navigator on my pokemon

--- src/pages/MyPokemon.js
+++ src/pages/MyPokemon.js
@@ -1,19 +1,91 @@
 import React from "react";
 
-import ListComponent from "../components/List";
+import { addOrRemoveClass, setScrollPosition } from "../helpers";
 
 import ErrorPage from "./Error";
 
+import ListComponent from "../components/List";
 import ContainerComponent from "../components/Container";
 import SkeletonComponent from "../components/Skeleton";
 import CaseComponent from "../components/Case";
 import ScreenComponent from "../components/Screen";
 import NavigationComponent from "../components/Navigation";
 
-function MyPokemon() {
+function MyPokemon({ history }) {
   const [ownedPokemons, setOwnedPokemons] = React.useState(() =>
     JSON.parse(localStorage.getItem("ownedPokemons"))
   );
+  const [selectedPokemon, setSelectedPokemon] = React.useState({});
+
+  function handleSelectedPokemon(e, pokemon, isSynthetic) {
+    // check if it was coming from synthetic based event
+    if (isSynthetic) {
+      e.preventDefault();
+    }
+    const target = isSynthetic ? e.target : e;
+    // remove class active from last selected pokemon
+    if (selectedPokemon.hasOwnProperty("lastTarget")) {
+      addOrRemoveClass(selectedPokemon.lastTarget, "remove", "active");
+    }
+    // then add class active to newly selected pokemon
+    addOrRemoveClass(target, "add", "active");
+
+    setSelectedPokemon({ lastTarget: target, pokemon });
+  }
+
+  function handleClickNavigation(e, direction) {
+    e.preventDefault();
+    let index = null;
+    const targetParent = document.querySelector("#My-Pokemon-List");
+    let target = null;
+    if (direction === "top") {
+      // if the user haven't selected a pokemon,
+      // else if the user already selected a pokemon
+      if (Object.keys(selectedPokemon).length === 0) {
+        index = ownedPokemons.length - 1;
+        target = targetParent.children[index];
+      } else {
+        for (let i = 0; i < targetParent.children.length; i++) {
+          if (targetParent.children[i].classList.contains("active")) {
+            index = i === 0 ? targetParent.children.length - 1 : i - 1;
+            target = targetParent.children[index];
+          }
+        }
+      }
+    } else if (direction === "bottom") {
+      if (Object.keys(selectedPokemon).length === 0) {
+        index = 0;
+        target = targetParent.children[index];
+      } else {
+        for (let i = 0; i < targetParent.children.length; i++) {
+          if (targetParent.children[i].classList.contains("active")) {
+            index = i === ownedPokemons.length - 1 ? 0 : i + 1;
+            target = targetParent.children[index];
+          }
+        }
+      }
+    }
+    // set the scroll position to the selected target's offset position,
+    // minus 32 because we have to adjust the offset according to the
+    // padding top and bottom that been placed which is 1rem + 1rem = 32px
+    setScrollPosition(targetParent, target.offsetTop - 32);
+    handleSelectedPokemon(target, ownedPokemons[index], false);
+  }
+
+  function goBack(e) {
+    e.preventDefault();
+
+    history.goBack();
+  }
+
+  function goToSelectedPokemon(e) {
+    e.preventDefault();
+
+    history.push({
+      pathname: `/pokemon_details/${selectedPokemon.pokemon.name}`,
+      state: selectedPokemon.pokemon.image,
+    });
+  }
 
   return (
     <>
@@ -37,6 +109,11 @@ function MyPokemon() {
                 </ul>
               </ScreenComponent>
             </CaseComponent>
+            <NavigationComponent
+              handleClickNavigation={handleClickNavigation}
+              goBack={goBack}
+              goToSelectedPokemon={goToSelectedPokemon}
+            />
           </SkeletonComponent>
         </ContainerComponent>
       )}
